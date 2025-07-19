de2d403cdb2b0dbff9c682c3859b12f108ff3475
Paul Kendrick <kenders2000@gmail.com>
2025-07-19 09:31:46 +0100

    Update home_cinema_control.py

--- home_cinema_control.py
+++ home_cinema_control.py
@@ -13,17 +13,54 @@ class CinemaRoomController:
             "stars_led": 21,
             "acoustic_panels": [16, 17, 18, 19, 20],
         }
-        self.node = ArtNetNode(ip)
-        self.universe = self.node.add_universe(universe)
-        self.steps = []
-        self._setup_lights()
+        # Each effect writes its value here
+        self.layers = {
+            "ambient": 0,
+            "sensor": 0,
+            "manual": 0
+            "max_ambient": 155,
+            "min_ambient": 30,
+        }
+        self.ambient_intensity_sequence =[0, 255, 0]
+        self.ambient_delay_sequence=[2, 2, 2]
 
-    def _setup_lights(self):
-        for i in range(self.num_steps):
-            dmx_start = i * self.channels_per_step + 1
-            dmx_channel = self.universe.add_channel(dmx_start, self.channels_per_step, byte_size=1)
-            dmx_channel.add_fade(0, 0, 0)  # Initialize off
-            self.steps.append(dmx_channel)
+    async def setup(self):
+        self.node = ArtNetNode(self.ip)
+        await self.node.start()
+        print("DMX initialized.")
+        self.universe = self.node.add_universe(universe)
+        self._setup_steps()
+        self._setup_panels()
+        
+    async def _setup_panels(self):
+        self.panels = []
+        for panel_n, channel in enumerate(self.channels["panels"]):
+            panel = self.universe.add_channel(start=channel, width=1)
+            panel.add_fade(0, 100)
+            self.panels.append(panel)
+        
+    async def _setup_steps(self):
+        self.steps = []
+        for step_n, channel in enumerate(self.channels["steps"]):
+            led = self.universe.add_channel(start=channel, width=1)  # 1 
+            led.add_fade(0, 100)
+            self.steps.append(led)
+            
+    async def _setup_stars(self):
+        self.stars = {}
+        self.stars["intensity"] = self.universe.add_channel(
+            start=self.channels["stars_intensity"], width=1
+        )
+        self.stars["speed"] = 
+
+    async def pulse_lights(self):
+        while self._running:
+            for value in self.ambient_intensity_sequence:
+                self.layers["ambient"] = value
+                await asyncio.sleep(2)
+
+
+
 
     async def set_step_color(self, step_idx, r, g, b, fade_time=0):
         if 0 <= step_idx < self.num_steps:
